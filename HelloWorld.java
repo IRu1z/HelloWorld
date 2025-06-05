358d331f5315ad54841571f60f6f584c846cfabd
IRu1z <ruizisaiah173@gmail.com>
2025-06-05 19:51:24 +0000

    Second save

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,14 @@
-public class HelloWorld
+public class HelloWorld // This establishes the class access and name of the class
 {
-    public static void main(String [] args)
+    public static void main(String [] args) /* The main method in a Java program represents the starting or entry point for executing a program */
     {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // Lines 5-10 each print out a different string of text.
         System.out.println("Isaiah Ruiz");
         System.out.println("Whataburger PattyMelt");
         System.out.println("Destroy Lonely");
         System.out.println("Cleaning cat litter");
-        System.out.println("I would rather be hanging out with my Friends");
+        System.out.println("I would rather be hanging out with my Friends"); 
+        /*System.out.println() is used to display output because it is the method so that allows you to produce an output in Java */
+        /*The purpose of comments in a program is for documentation */
     }
 }
