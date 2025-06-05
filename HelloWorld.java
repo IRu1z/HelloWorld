e84f1e8835ccc16a038b7e594e09d98f6879accf
IRu1z <ruizisaiah173@gmail.com>
2025-06-05 19:49:32 +0000

    First save

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Isaiah Ruiz");
+        System.out.println("Whataburger PattyMelt");
+        System.out.println("Destroy Lonely");
+        System.out.println("Cleaning cat litter");
+        System.out.println("I would rather be hanging out with my Friends");
+    }
+}
